c650f0868e1f45a5451c214dbcfcda6e1e0cb980
gururk2@gmail.com <gururk2@gmail.com>
2015-10-07 11:51:51 +0530

    new update

---
+++ connect.php
@@ -0,0 +1,6 @@
+<?php
+$con=mysqli_connect ("localhost","root","","vtu");
+if (mysqli_connect_errno()) {
+echo "failed to connect mysql: ". mysqli_connect_error();
+}
+?>
---
+++ footer.php
@@ -0,0 +1,3 @@
+<div id="footer">Created for Nettuts+</div>
+</body>
+</html>
